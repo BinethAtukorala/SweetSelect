0c66619b267e7b168aa9eaa572ab09b35c9d224f
BihanSudusinghe <b.y.sudusinghe@gmail.com>
2024-09-21 19:25:36 +1000

    Created robot model for LBR iiwa 7 R800

---
+++ LBRiiwa.m
@@ -0,0 +1,44 @@
+classdef LBRiiwa < RobotBaseClass
+    %%  Kuka LBR iiwa 7 R800 robot model
+    properties(Access = public)   
+        plyFileNameStem = 'LBRiiwa';
+    end 
+    
+    methods
+%% Constructor
+        function self = LBRiiwa(baseTr)
+			self.CreateModel();
+            if nargin < 1			
+				baseTr = eye(4);				
+            end
+            % self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
+            self.model.base = self.model.base.T * baseTr;
+            self.PlotAndColourRobot();         
+        end
+
+%% CreateModel
+        function CreateModel(self)
+            link(1) = Link('d',0.340,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            link(2) = Link('d',0,'a',0,'alpha',pi/2,'qlim', deg2rad([-360 360]), 'offset',0);
+            link(3) = Link('d',0.400,'a',0,'alpha',pi/2,'qlim', deg2rad([-360 360]), 'offset', 0);
+            link(4) = Link('d',0,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            link(5) = Link('d',0.400,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(6) = Link('d',	0,'a',0,'alpha',pi/2,'qlim',deg2rad([-360,360]), 'offset', 0);
+            link(7) = Link('d',	0.126,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+            
+            % Incorporate joint limits
+            link(1).qlim = [-170 170]*pi/180;
+            link(2).qlim = [-120 120]*pi/180;
+            link(3).qlim = [-170 170]*pi/180;
+            link(4).qlim = [-120 120]*pi/180;
+            link(5).qlim = [-170 170]*pi/180;
+            link(6).qlim = [-120 120]*pi/180;
+            link(7).qlim = [-175 175]*pi/180;
+
+            % link(3).offset = -pi/2;
+            % link(5).offset = -pi/2;     
+             
+            self.model = SerialLink(link,'name',self.name);
+        end      
+    end
+end
