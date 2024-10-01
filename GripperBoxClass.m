159bed94ba5ce1387691b0f0841e7d22b139a5c4
Binada Nethmin Sudusinghe <binadasudusinghe@Binadas-MacBook-Pro.local>
2024-10-01 21:01:50 +1000

    Added functions to open and close the box gripper, keep box gripper stationary and set box gripper base

--- GripperBoxClass.m
+++ GripperBoxClass.m
@@ -12,5 +12,60 @@ classdef GripperBoxClass
             obj.Box_Right_Finger = GripperBoxRight(transl([0, 0, 0]));
             obj.Box_Left_Finger = GripperBoxLeft(transl([0, 0, 0]));
         end
+
+        % function animateGripper(obj, Right_Trajectory, Left_Trajectory)
+        %     for i = 1:size(Right_Trajectory, 1)
+        %         % Animate right finger
+        %         obj.Box_Right_Finger.model.animate(Right_Trajectory(i, :));
+        %         % Animate left finger
+        %         obj.Box_Left_Finger.model.animate(Left_Trajectory(i, :));
+        %         pause(0);
+        %     end
+        % end
+        
+
+        function openGripper(obj)
+            % Trajectories for fingers to move towards each other
+            Right_Trajectory = jtraj(obj.Box_Right_Finger.model.getpos(), [0, -pi/4], 50);
+            Left_Trajectory = jtraj(obj.Box_Left_Finger.model.getpos(), [0, pi/4], 50);
+            for i = 1:size(Right_Trajectory, 1)
+                % Animate right finger
+                obj.Box_Right_Finger.model.animate(Right_Trajectory(i, :));
+                % Animate left finger
+                obj.Box_Left_Finger.model.animate(Left_Trajectory(i, :));
+                pause(0);
+            end
+        end
+        
+
+        function closeGripper(obj)
+            % Trajectories for fingers to move away from each other
+            Right_Trajectory = jtraj(obj.Box_Right_Finger.model.getpos(), [0, pi/16], 50);
+            Left_Trajectory = jtraj(obj.Box_Left_Finger.model.getpos(), [0, -pi/16], 50);
+            for i = 1:size(Right_Trajectory, 1)
+                % Animate right finger
+                obj.Box_Right_Finger.model.animate(Right_Trajectory(i, :));
+                % Animate left finger
+                obj.Box_Left_Finger.model.animate(Left_Trajectory(i, :));
+                pause(0);
+            end
+        end
+
+
+        function stationaryGripper(obj)
+
+            Right_Trajectory = jtraj(obj.Box_Right_Finger.model.getpos(), obj.Box_Right_Finger.model.getpos(), 2);
+            Left_Trajectory = jtraj(obj.Box_Left_Finger.model.getpos(), obj.Box_Left_Finger.model.getpos(), 2);
+            
+            % Animate both fingers at their current positions
+            obj.Box_Right_Finger.model.animate(Right_Trajectory(1,:));
+            obj.Box_Left_Finger.model.animate(Left_Trajectory(1,:));
+
+        end
+        
+        function setGripperBase(obj, EndEffector_Pose)
+            obj.Box_Right_Finger.model.base = EndEffector_Pose;
+            obj.Box_Left_Finger.model.base = EndEffector_Pose;
+        end
     end
 end
