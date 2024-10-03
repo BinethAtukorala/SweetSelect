3f85c98bfcf6b4e371045a7e1205bab7c4653915
Binada Nethmin Sudusinghe <binadasudusinghe@Binadas-MacBook-Pro.local>
2024-10-03 23:36:42 +1000

    Added candy positions and created and placed dummy candies

--- Main.m
+++ Main.m
@@ -43,7 +43,74 @@ end
 
 
 % Place candies
-CandyClass.placeCandy();
+Raspberry = [ 
+    1.21, 0.7, 0.6;
+    1.21, 0.75, 0.6;
+    1.21, 0.85, 0.6;
+];
+
+Blueberry = [                                         
+    1.15, 1, 0.6;
+    1.15, 1.05, 0.6;
+    1.15, 1.1, 0.6;
+    1.15, 1.15, 0.6;
+];
+Greenapple = [                                         
+    1.21, 0.9, 0.6;
+    1.21, 0.95, 0.6;
+    1.21, 1, 0.6;
+    1.21, 1.05, 0.6;
+];
+
+
+Raspberry_Candy = [];
+
+for i = 1:size(Raspberry, 1)
+    Raspberry_Candy = [Raspberry_Candy, CandyClass(Raspberry_Candy(i, :),"Raspberry")];
+end
+
+Blueberry_Candy = [];
+
+for i = 1:size(Raspberry, 1)
+    Blueberry_Candy = [Blueberry_Candy, CandyClass(Blueberry_Candy(i, :),"Blueberry")];
+end
+
+Greenapple_Candy = [];
+
+for i = 1:size(Raspberry, 1)
+    Greenapple_Candy = [Greenapple_Candy, CandyClass(Greenapple_Candy(i, :),"Greenapple")];
+end
+
+% Place dummy candy
+Dummy_Raspberry = [ 
+    1.21, 0.8, 0.6;
+];
+
+Dummy_Blueberry = [                                         
+    1.15, 0.7, 0.6;
+    1.15, 0.75, 0.6;
+    1.15, 0.8, 0.6;
+    1.15, 0.85, 0.6;
+    1.15, 0.9, 0.6;
+    1.15, 0.95, 0.6;
+];
+Dummy_Greenapple = [                                         
+    1.21, 1.1, 0.6;
+    1.21, 1.15, 0.6;
+];
+
+for i = 1:size(Dummy_Raspberry, 1)
+    PlaceObject('candyballraspberry.ply', Dummy_Raspberry(i, :));
+end
+
+for i = 1:size(Dummy_Blueberry, 1)
+    PlaceObject('candyballblueberry.ply', Dummy_Blueberry(i, :));
+end
+
+for i = 1:size(Dummy_Greenapple, 1)
+    PlaceObject('candyballgreenapple.ply', Dummy_Greenapple(i, :));
+end
+
 
 % Keep grippers open initially
 Box_Gripper.openGripper();
