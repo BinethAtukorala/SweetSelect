ba84cb629d659778c0d07fb41e2d36eeb248c5f1
Bineth Atukorala <bineth.mandiv@gmail.com>
2024-10-07 20:47:40 +1100

    Added Maintaincence tab

---
+++ MainProgram.m
@@ -0,0 +1,29 @@
+classdef MainProgram
+    properties
+        r % Property to store the robot object model
+    end
+
+    methods
+        %% Constructor to initialize the robot model
+        function obj = MainProgram()
+            % Initialize the robot model UR3e
+            % The robot is placed at a translational offset on top of the table
+            obj.r  = UR3e();
+
+        end
+
+        function putCandiesInBox(obj, A, B, C)
+            fprintf("Candy A: %d\nCandy B: %d\nCandy C: %d\n", A, B, C)
+        end
+
+        function result = getQLims(obj)
+            result = obj.r.model.qlim;
+        end
+
+        function setQValues(obj, q)
+            obj.r.model.animate(q);
+        end
+
+    end
+
+end
