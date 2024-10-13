dee64af9dbfb9144b949012ce4e1a951380330c7
Bihan Sudusinghe <b.y.sudusinghe@gmail.com>
2024-10-13 17:23:42 +1100

    Edited function for vertices transformations

--- CandyClass.m
+++ CandyClass.m
@@ -19,9 +19,11 @@ classdef CandyClass
             obj.Vertices = get(obj.Candy_Object, 'Vertices');
         end
         
-        function updateCandyPosition(obj, Transformation, Vertices)
-            New_Vertices = (Transformation(1:3, 1:3) * Vertices' + Transformation(1:3, 4))';
-            set(Placed_Candy, 'Vertices', New_Vertices);
+        function updateCandyPosition(obj, Transformation)
+            New_Vertices = (Transformation(1:3, 1:3) * obj.Vertices' + Transformation(1:3, 4))';
+            set(obj.Candy_Object, 'Vertices', New_Vertices);
+            % New_Vertices = (Transformation(1:3, 1:3) * obj.Vertices' + Transformation(1:3, 4))';
+            % set(obj.Candy_Object, 'Vertices', New_Vertices);
             % Gotta edit this so that it checks what the moving candy is, and then changes that candys vertices
             % For example the "Placed_Candy" above should be either Placed_Green_Apple_Candy, etc
         end
