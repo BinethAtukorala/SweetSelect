53c90bba3949bca9a6d7945d42ac21ee12fd919e
Bihan Sudusinghe <b.y.sudusinghe@gmail.com>
2024-10-15 19:33:34 +1100

    Corrected some errors

--- SweetSelectMain.m
+++ SweetSelectMain.m
@@ -25,13 +25,6 @@ LBRiiwa = LBRiiwaClass(Box_Gripper);
 % Create an instance of the UR3Class, representing the UR3 model
 UR3e = UR3eClass(Candy_Gripper);
 
-% % Place grippers on robot end effectors
-% LBRiiwa_EndEffector_Pose = LBRiiwa.model.fkine(LBRiiwa.model.getpos()).T;
-% Box_Gripper.setGripperBase(LBRiiwa_EndEffector_Pose);
-% 
-% UR3e_EndEffector_Pose = UR3e.model.fkine(UR3e.model.getpos()).T;
-% Box_Gripper.setGripperBase(UR3e_EndEffector_Pose);
-
 % Keep grippers open initially
 Box_Gripper.openGripper();
 Candy_Gripper.openGripper();
@@ -58,9 +51,9 @@ Greenapple = [
 ];
 
 % Edit candy count here
-Raspberry_Count = 4;
-Blueberry_Count = 4;
-Greenapple_Count = 4;
+Raspberry_Count = 2;
+Blueberry_Count = 2;
+Greenapple_Count = 1;
     
 Candy_Initial_Poses = [
     Raspberry(1:Raspberry_Count, :);
@@ -85,13 +78,29 @@ for i = 1:size(Candy_Initial_Poses, 1)
     end
 end
 
+% Place remaining unselected candies using PlaceObject
+% Unselected Raspberry candies
+for i = Raspberry_Count+1:size(Raspberry, 1)
+    PlaceObject('candyBallRaspberry.ply', Raspberry(i, :));
+end
+
+% Unselected Blueberry candies
+for i = Blueberry_Count+1:size(Blueberry, 1)
+    PlaceObject('candyBallBlueberry.ply', Blueberry(i, :));
+end
+
+% Unselected Greenapple candies
+for i = Greenapple_Count+1:size(Greenapple, 1)
+    PlaceObject('candyBallGreenApple.ply', Greenapple(i, :));
+end
+
 Candy_Final_Poses = [                                         
     0.48, 1, 0.95;
     0.54, 1, 0.95;
     0.60, 1, 0.95;
 ];
 
-Candy_Final_Poses(:,3) = Candy_Final_Poses(:,3) + 0.1;
+Candy_Final_Poses(:,3) = Candy_Final_Poses(:,3) + 0.08;
 
 %% Boxes
 Front_Box_Poses = [
@@ -105,33 +114,49 @@ Back_Box_Poses = [
 ];
 
 % Calculate how many boxes are needed
-Num_Boxes = ceil((Raspberry_Count + Blueberry_Count + Greenapple_Count)/ 3)
+Num_Boxes = ceil((Raspberry_Count + Blueberry_Count + Greenapple_Count)/ 3);
 
 Boxes = [];
 Box_Initial_Poses = [];
 
 % If 1 or 2 boxes are needed, select from the front
 if Num_Boxes <= 2
-    for i = 1:size(Num_Boxes, 1)
+    for i = 1:Num_Boxes
         Boxes = [Boxes, BoxClass(Front_Box_Poses(i, :))];
-        Box_Initial_Poses = [Box_Initial_Poses; Front_Box_Poses(i, :)]
+        Box_Initial_Poses = [Box_Initial_Poses; Front_Box_Poses(i, :)];
     end
 % If more than 2 boxes are needed, take 2 from the front and the rest from the back
 elseif Num_Boxes > 2
     % If more than 2 boxes are needed, take 2 from the front and the rest from the back
     for a = 1:2
         Boxes = [Boxes, BoxClass(Front_Box_Poses(a, :))];
-        Box_Initial_Poses = [Box_Initial_Poses; Front_Box_Poses(a, :)]
+        Box_Initial_Poses = [Box_Initial_Poses; Front_Box_Poses(a, :)];
     end
 
     for b = 1:(Num_Boxes - 2)
         Boxes = [Boxes, BoxClass(Back_Box_Poses(b, :))];
-        Box_Initial_Poses = [Box_Initial_Poses; Back_Box_Poses(b, :)]
+        Box_Initial_Poses = [Box_Initial_Poses; Back_Box_Poses(b, :)];
+    end
+end
+
+% If all boxes are selected, skip the unselected box placement
+if Num_Boxes < size(Front_Box_Poses, 1)
+    for i = Num_Boxes + 1:size(Front_Box_Poses, 1)
+        PlaceObject('box.ply', Front_Box_Poses(i, :));
+    end
+
+    for i = 1:size(Back_Box_Poses, 1)
+        PlaceObject('box.ply', Back_Box_Poses(i, :));
+    end
+end
+
+if Num_Boxes >= 2 && (Num_Boxes - 2) < size(Back_Box_Poses, 1)
+    for i = Num_Boxes - 2 + 1:size(Back_Box_Poses, 1)
+        PlaceObject('box.ply', Back_Box_Poses(i, :));
     end
 end
 
 %% Movement
-
 
 % Get the total number of candies
 Num_Candies = size(Candy_Initial_Poses, 1);
@@ -163,77 +188,9 @@ for Box_Index = 1:Num_Boxes
             [Candy_Start_Pose] = UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(x,:), Candy_Final_Poses, Candies(x), x);
             Candy_Start_Poses = [Candy_Start_Poses; Candy_Start_Pose];
         end
-
         LBRiiwa.moveBackFromMidway(LBRiiwa, Box_Gripper, Boxes(Box_Index), Box_Index, Candies(Start_Index:End_Index), Box_Start_Pose, Candy_Start_Poses);
 
     end
 end
-
-
-
-% if size(Candy_Initial_Poses) <= 3
-%     for z = 1:size(Candy_Initial_Poses)
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Selected_Boxes(1));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(z,:), Candy_Final_Poses, Candies(z,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(1))
-%     end
-% 
-% elseif size(Candy_Initial_Poses) > 3 && size(Candy_Initial_Poses) <= 6
-%     for y = 1:3
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Selected_Boxes(1));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(y,:), Candy_Final_Poses, Candies(y,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(1))
-%     end
-% 
-%     for z = 3:size(Candy_Initial_Poses)
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(2));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(z,:), Candy_Final_Poses, Candies(z,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(2))
-%     end
-% 
-% elseif size(Candy_Initial_Poses) > 6 && size(Candy_Initial_Poses) <= 9
-%     for x = 1:3
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(1));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(x,:), Candy_Final_Poses, Candies(x,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(1))
-%     end
-% 
-%     for y = 3:6
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(2));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(y,:), Candy_Final_Poses, Candies(y,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(2))
-%     end
-% 
-%     for z = 6:size(Candy_Initial_Poses)
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(3));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(z,:), Candy_Final_Poses, Candies(z,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(3))
-%     end
-% 
-% elseif size(Candy_Initial_Poses) > 9 && size(Candy_Initial_Poses) <= 12
-%     for w = 1:3
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(1));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(w,:), Candy_Final_Poses, Candies(w,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(1))
-%     end
-% 
-%     for x = 3:6
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(2));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(x,:), Candy_Final_Poses, Candies(x,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(2))
-%     end
-% 
-%     for x = 6:9
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(3));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(y,:), Candy_Final_Poses, Candies(y,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(3))
-%     end
-% 
-%     for z = 6:size(Candy_Initial_Poses)
-%         LBRiiwa.moveFrontToMidway(LBRiiwa, Box_Gripper, Initial_Box_Pose, Selected_Boxes(4));
-%         UR3e.moveUR3e(UR3e, Candy_Gripper, Candy_Initial_Poses(z,:), Candy_Final_Poses, Candies(z,:))
-%         LBRiiwa.moveFrontFromMidway(obj, LBRiiwa, Box_Gripper, Selected_Boxes(4))
-%     end
-% end
 
 
