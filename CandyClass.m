6d25786bf778aa471bdfc259c9ba548b86a3bf29
Binada Nethmin Sudusinghe <binadasudusinghe@Binadas-MacBook-Pro.local>
2024-10-15 19:42:00 +1100

    Removed some unnecessary comments

--- CandyClass.m
+++ CandyClass.m
@@ -22,10 +22,6 @@ classdef CandyClass
         function updateCandyPosition(obj, Transformation)
             New_Vertices = (Transformation(1:3, 1:3) * obj.Vertices' + Transformation(1:3, 4))';
             set(obj.Candy_Object, 'Vertices', New_Vertices);
-            % New_Vertices = (Transformation(1:3, 1:3) * obj.Vertices' + Transformation(1:3, 4))';
-            % set(obj.Candy_Object, 'Vertices', New_Vertices);
-            % Gotta edit this so that it checks what the moving candy is, and then changes that candys vertices
-            % For example the "Placed_Candy" above should be either Placed_Green_Apple_Candy, etc
         end
     end
 end
