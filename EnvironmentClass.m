f9090d87eb80444376bc038a9fb585d681cf3f35
Binada Nethmin Sudusinghe <binadasudusinghe@Binadas-MacBook-Pro.local>
2024-10-23 16:15:58 +1100

    Changed the environment slightly

--- EnvironmentClass.m
+++ EnvironmentClass.m
@@ -9,19 +9,19 @@ classdef EnvironmentClass
             % Adding a .ply file as the flat background surface (Code 
             % taken from Canvas in Lab Assignment 1 submission page) 
             surf([-2, -2; 2, 2], ...               % X coordinates
-                 [-2, 2; -2, 2], ...               % Y coordinates
+                 [-2, 2.5; -2, 2.5], ...               % Y coordinates
                  [0.0, 0.0; 0.0, 0.0], ...                 % Z coordinates 
                  'CData', imread('pinktiles.jpg'), ...            
                  'FaceColor', 'texturemap');      
             
-            tableRobot = PlaceObject('tableRobot.ply', [0, 1, 0]); 
-            tableServe = PlaceObject('tableServe.ply', [0, 0.25, 0]); 
-            
-            % candyShelf1 = PlaceObject('candyShelf.ply', [1, 1.75, 0]);
-            % candyShelf2 = PlaceObject('candyShelf.ply', [-1, 1.75, 0]);
-            % candyBox1 = PlaceObject('candyBox.ply', [1, 0.75, 0.5]);
-            % candyBox2 = PlaceObject('candyBox.ply', [1, 1, 0.5]);
-            % candyBox3 = PlaceObject('candyBox.ply', [1, 1.25, 0.5]);
+            tableRobot = PlaceObject('tableRobot.ply', [0, 1.2, 0]); 
+            tableServe = PlaceObject('tableServe.ply', [0, 0.25, 0]);
+            candyShelf1 = PlaceObject('candyShelf.ply', [1, 2.25, 0]);
+            candyShelf2 = PlaceObject('candyShelf.ply', [-1, 2.25, 0]);
+            % candytray = PlaceObject('tray.ply', [0.65, 1.575, 0.8]);
+            boxRobot = PlaceObject('boxRobot.ply', [0.74, 1.25, 0.5]);
+            boxBox = PlaceObject('boxBox.ply', [-1.15, 1, 0.5]);
+            boxCandy = PlaceObject('boxCandy.ply', [-0.2, 0.2, 0.5]);
             creditCardReader = PlaceObject('creditCardReader.ply', [-1.2, 0.1, 0.5]);
             monitor = PlaceObject('monitor.ply', [-1.5, 0.1, 0.5]);
 
