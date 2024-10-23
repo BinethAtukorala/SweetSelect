b866e1f03dfc6d95267b72bcbe538f92beb39302
Bihan Sudusinghe <b.y.sudusinghe@gmail.com>
2024-10-23 18:37:47 +1100

    Added jars to hold candy

--- EnvironmentClass.m
+++ EnvironmentClass.m
@@ -14,16 +14,31 @@ classdef EnvironmentClass
                  'CData', imread('pinktiles.jpg'), ...            
                  'FaceColor', 'texturemap');      
             
-            tableRobot = PlaceObject('tableRobot.ply', [0, 1.2, 0]); 
-            tableServe = PlaceObject('tableServe.ply', [0, 0.25, 0]);
-            candyShelf1 = PlaceObject('candyShelf.ply', [1, 2.25, 0]);
-            candyShelf2 = PlaceObject('candyShelf.ply', [-1, 2.25, 0]);
+            % tableRobot = PlaceObject('tableRobot.ply', [0, 1.2, 0]); 
+            % tableServe = PlaceObject('tableServe.ply', [0, 0.25, 0]);
+            % candyShelf1 = PlaceObject('candyShelf.ply', [1, 2.25, 0]);
+            % candyShelf2 = PlaceObject('candyShelf.ply', [-1, 2.25, 0]);
             % candytray = PlaceObject('tray.ply', [0.65, 1.575, 0.8]);
-            boxRobot = PlaceObject('boxRobot.ply', [0.74, 1.25, 0.5]);
-            boxBox = PlaceObject('boxBox.ply', [-1.15, 1, 0.5]);
-            boxCandy = PlaceObject('boxCandy.ply', [-0.2, 0.2, 0.5]);
-            creditCardReader = PlaceObject('creditCardReader.ply', [-1.2, 0.1, 0.5]);
-            monitor = PlaceObject('monitor.ply', [-1.5, 0.1, 0.5]);
+            % boxRobot = PlaceObject('boxRobot.ply', [0.74, 1.25, 0.5]);
+            % boxBox = PlaceObject('boxBox.ply', [-1.15, 1, 0.5]);
+            % boxCandy = PlaceObject('boxCandy.ply', [-0.2, 0.2, 0.5]);
+            % creditCardReader = PlaceObject('creditCardReader.ply', [-1.2, 0.1, 0.5]);
+            % monitor = PlaceObject('monitor.ply', [-1.5, 0.1, 0.5]);
+
+            Table_Robot = PlaceObject('tableRobot.ply', [0, 1.2, 0]); 
+            Table_Serve = PlaceObject('tableServe.ply', [0, 0.25, 0]);
+            Candy_Shelf_1 = PlaceObject('candyShelf.ply', [1, 2.25, 0]);
+            Candy_Shelf_2 = PlaceObject('candyShelf.ply', [-1, 2.25, 0]);
+            % candytray = PlaceObject('tray.ply', [0.65, 1.575, 0.8]);
+            Stage_Robot = PlaceObject('boxRobot.ply', [0.74, 1.25, 0.5]);
+            Stage_Box = PlaceObject('boxBox.ply', [-1.15, 1, 0.5]);
+            Stage_Candy = PlaceObject('boxCandy.ply', [-0.2, 0.2, 0.5]);
+            Credit_Card_Reader = PlaceObject('creditCardReader.ply', [-1.2, 0.1, 0.5]);
+            Monitor = PlaceObject('monitor.ply', [-1.5, 0.1, 0.5]);
+
+            Raspberry_Jar = PlaceObject('raspberryJar.ply', [0.5, 1.6, 0.8]);
+            Blueberry_Jar = PlaceObject('blueberryJar.ply', [0.8, 1.6, 0.8]);
+            Greenapple_Jar = PlaceObject('greenappleJar.ply', [0.65, 1.6, 0.8]);
 
         end
     end
